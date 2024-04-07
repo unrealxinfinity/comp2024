5035bc02a8f506e23a6bdea56810e0688b4cdb90
Onso37 <afonso.vo@gmail.com>
2024-04-07 10:20:00 +0100

    start making a pass to define all types

--- src/main/pt/up/fe/comp2024/analysis/JmmAnalysisImpl.java
+++ src/main/pt/up/fe/comp2024/analysis/JmmAnalysisImpl.java
@@ -7,6 +7,7 @@ import pt.up.fe.comp.jmm.ast.JmmNode;
 import pt.up.fe.comp.jmm.parser.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2024.analysis.passes.OperandsMismatch;
 import pt.up.fe.comp2024.analysis.passes.UndeclaredVariable;
 import pt.up.fe.comp2024.symboltable.JmmSymbolTableBuilder;
 
@@ -20,7 +21,7 @@ public class JmmAnalysisImpl implements JmmAnalysis {
 
     public JmmAnalysisImpl() {
 
-        this.analysisPasses = List.of(new UndeclaredVariable());
+        this.analysisPasses = List.of(new UndeclaredVariable(), new OperandsMismatch());
 
     }
 
