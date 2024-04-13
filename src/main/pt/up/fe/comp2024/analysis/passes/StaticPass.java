8a073c630a24f5841b416ed3b6b9d55ce653a9f8
Onso37 <afonso.vo@gmail.com>
2024-04-13 16:19:51 +0100

    add a report

--- src/main/pt/up/fe/comp2024/analysis/passes/StaticPass.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/StaticPass.java
@@ -9,6 +9,8 @@ import pt.up.fe.comp2024.analysis.AnalysisVisitor;
 import pt.up.fe.comp2024.ast.Kind;
 import pt.up.fe.comp2024.ast.NodeUtils;
 
+import java.util.Optional;
+
 public class StaticPass extends AnalysisVisitor {
     
     protected void buildVisitor() {
@@ -16,9 +18,15 @@ public class StaticPass extends AnalysisVisitor {
     }
 
     private Void checkThisUsage(JmmNode jmmNode, SymbolTable symbolTable) {
-        JmmNode methodDecl = jmmNode.getAncestor(Kind.METHOD_DECL).get();
-        if (methodDecl.getObject("isStatic", Boolean.class)) {
-            String message = String.format("'this' used in a non-static context in method %s", methodDecl.get("name"));
+        Optional<JmmNode> methodDecl = jmmNode.getAncestor(Kind.METHOD_DECL);
+        if (methodDecl.isEmpty()) {
+            String message = "'this' used outside a method";
+            Report report = NodeUtils.createSemanticError(jmmNode, message);
+            addReport(report);
+            return null;
+        }
+        if (methodDecl.get().getObject("isStatic", Boolean.class)) {
+            String message = String.format("'this' used in a non-static context in method %s", methodDecl.get().get("name"));
             Report report = NodeUtils.createSemanticError(jmmNode, message);
             addReport(report);
         }
